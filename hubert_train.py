7de9306a59e917e3ba850b2f2b60a31f4a0bb8f7
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2025-12-02 16:17:48 +0900

    fix bugs

--- hubert_train.py
+++ hubert_train.py
@@ -1,3 +1,9 @@
+import warnings
+
+# pkg_resourcesの非推奨警告を抑制 (lightning importより前に設定)
+warnings.filterwarnings('ignore', 'pkg_resources is deprecated as an API', UserWarning)
+warnings.filterwarnings('ignore')  # 他の警告も引き続き抑制
+
 import os
 import torch
 import torch.multiprocessing as mp
@@ -12,10 +18,6 @@ import pandas as pd
 import yaml
 from argparse import ArgumentParser
 from string import Template
-import warnings
-# pkg_resourcesの非推奨警告を抑制
-warnings.filterwarnings('ignore', 'pkg_resources is deprecated as an API', UserWarning)
-warnings.filterwarnings('ignore') # 他の警告も引き続き抑制
 
 def load_config(path: str) -> dict:
     raw = open(path, 'r', encoding='utf-8').read()
